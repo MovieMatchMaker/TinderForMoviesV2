33e11829f31062d5e010f22a7f58f6bccf5105e9
Thomas <nyumat18@gmail.com>
2022-09-27 12:27:16 -0700

    Testing

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors())
 
 
 app.use(express.static('client/build'));
-const path = require('path');
+import path from "path";
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	}
